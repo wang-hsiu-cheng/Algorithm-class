d85e3bee7b605ed425801569358c53ac703a9ad7
TWang <wang.tw.ted@gmail.com>
2025-03-08 22:54:27 +0800

    add: O(n) plot

--- draw_plot.py
+++ draw_plot.py
@@ -11,6 +11,14 @@ algorithm_time = [[0 for i in range(data_columns-1)] for j in range(data_rows-1)
 data_numbers = [0 for i in range(data_columns-1)]
 algorithm_name = [0 for i in range(data_rows-1)]
 
+Start_t = int(10/0.001)
+End_t = int(2560/0.001)
+t = np.linspace(Start_t * 0.001, End_t * 0.001, End_t - Start_t + 1)
+
+reference = np.zeros(End_t - Start_t + 1)
+for i in range(End_t - Start_t + 1):
+    reference[i] = 0.0000000015 * i * 0.001 + 0.00000001
+
 for i in range(2, data_rows+1):
     for j in range(2, data_columns+1):
         algorithm_time[i-2][j-2] = result_data.cell(row=i, column=j).value
@@ -21,6 +29,7 @@ for i in range(2, data_rows+1):
 
 # set graph style
 plt.figure(figsize=(8,6))
+plt.plot(t, reference, color="C8", label="O(n)", linewidth=3)
 plt.plot(data_numbers, algorithm_time[0], color="gray", label=algorithm_name[0], linewidth=3)
 plt.plot(data_numbers, algorithm_time[1], color="orange", label=algorithm_name[1], linewidth=3)
 plt.plot(data_numbers, algorithm_time[2], color="blue", label=algorithm_name[2], linewidth=3)
